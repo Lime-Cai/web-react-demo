c2404a198618a979b0e7c6cb21a8f13981e05569
Lime <zong64066406@gmail.com>
2023-10-26 15:42:10 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import Input from './components/Input';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [text, setText] = useState('');
+
+    const onChangeHandler = (e) => {
+        setText(e.target.value);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                {text}
+                <Input
+                    id="sampleText"
+                    text="這是個Inputr"
+                    value={text}
+                    onChangeHandler={onChangeHandler}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
