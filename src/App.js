8fb3ff0df27abf35d4042b552f666c95c0c63196
Lime <zong64066406@gmail.com>
2023-10-31 03:06:47 +0800

    CRA整合製作簡易購物車 - 版型製作

--- src/App.js
+++ src/App.js
@@ -8,42 +8,129 @@ import Input from './components/Input';
 import './assets/all.scss'
 
 function App() {
-    const [text, setText] = useState('');
-
-    const onChangeHandler = (e) => {
-        setText(e.target.value);
-    }
-    useEffect(() => {
-        (async ()=>{
-            const result = await axios.get('https://randomuser.me/api/');
-            axios.get('https://randomuser.me/api/');
-            console.log(result)
-        })();
-    }, []);
 
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                {text}
-                <button type="button" className="btn btn-primary">Primary</button>
-                <Input
-                    id="sampleText"
-                    text="這是個Inputr"
-                    value={text}
-                    onChangeHandler={onChangeHandler}/>
-            </header>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <span className="navbar-brand">甜點蛋糕店</span>
+                    <button className="btn btn-outline-dark position-relative" type="submit">
+                        購物車
+                        <span
+                            className="badge text-bg-danger position-absolute top-0 start-100 translate-middle">99</span>
+                    </button>
+
+                </div>
+            </nav>
+            <div className="container mt-4">
+                {/* 外層格線 */}
+                <div className="row">
+                    {/* 加起來一定會是12 */}
+                    <div className="col-md-7">
+                        {/* 內層格線 */}
+                        <div className="row row-cols-3 g-2 ">
+                            <div className="col">
+                                <div className="card">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
+                                        className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            <span className='float-start'>全素食健康餐</span>
+                                            <span className='float-end'>NT$ 220</span>
+                                        </h5>
+                                        <button type='type' className="btn btn-outline-primary w-100 mt-2">
+                                            加入購物車
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
+                                        className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            <span className='float-start'>全素食健康餐</span>
+                                            <span className='float-end'>NT$ 220</span>
+                                        </h5>
+                                        <button type='type' className="btn btn-outline-primary w-100 mt-2">
+                                            加入購物車
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
+                                        className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            <span className='float-start'>全素食健康餐</span>
+                                            <span className='float-end'>NT$ 220</span>
+                                        </h5>
+                                        <button type='type' className="btn btn-outline-primary w-100 mt-2">
+                                            加入購物車
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
+                                        className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">
+                                            <span className='float-start'>全素食健康餐</span>
+                                            <span className='float-end'>NT$ 220</span>
+                                        </h5>
+                                        <button type='type' className="btn btn-outline-primary w-100 mt-2">
+                                            加入購物車
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-5">
+                        <div className="bg-light p-3">
+                            <table className='table align-middle'>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <a href="#">x</a>
+                                        </td>
+                                        <td>
+                                            <img src="" alt=""/>
+                                        </td>
+                                        <td>
+                                            素食餐
+                                            <br/>
+                                            <small className="text-muted">NT$ 220</small>
+                                        </td>
+                                        <td>
+                                            <select name="" id="" className="form-select"></select>
+                                        </td>
+                                        <td className='text-end'>
+                                            NT$ 440
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <td colSpan={5} className='text-end'>
+                                            總金額 NT$ 440
+                                        </td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
