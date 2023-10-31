9076204ae1f4f97e6106a35d7af1ad64c91a616f
operator48 <zong64066406@gmail.com>
2023-10-31 22:33:34 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -35,10 +35,26 @@ function App() {
                                         src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
                                         className="card-img-top" alt="..."/>
                                     <div className="card-body">
-                                        <h5 className="card-title">
+                                        <h6 className="card-title">
                                             <span className='float-start'>全素食健康餐</span>
                                             <span className='float-end'>NT$ 220</span>
-                                        </h5>
+                                        </h6>
+                                        <button type='type' className="btn btn-outline-primary w-100 mt-2">
+                                            加入購物車
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col">
+                                <div className="card">
+                                    <img
+                                        src="https://images.unsplash.com/photo-1698688334089-c68105801d02?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                        className="card-img-top" alt="..."/>
+                                    <div className="card-body">
+                                        <h6 className="card-title">
+                                            <span className='float-start'>全素食健康餐</span>
+                                            <span className='float-end'>NT$ 220</span>
+                                        </h6>
                                         <button type='type' className="btn btn-outline-primary w-100 mt-2">
                                             加入購物車
                                         </button>
@@ -51,10 +67,10 @@ function App() {
                                         src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
                                         className="card-img-top" alt="..."/>
                                     <div className="card-body">
-                                        <h5 className="card-title">
+                                        <h6 className="card-title">
                                             <span className='float-start'>全素食健康餐</span>
                                             <span className='float-end'>NT$ 220</span>
-                                        </h5>
+                                        </h6>
                                         <button type='type' className="btn btn-outline-primary w-100 mt-2">
                                             加入購物車
                                         </button>
@@ -67,26 +83,10 @@ function App() {
                                         src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
                                         className="card-img-top" alt="..."/>
                                     <div className="card-body">
-                                        <h5 className="card-title">
+                                        <h6 className="card-title">
                                             <span className='float-start'>全素食健康餐</span>
                                             <span className='float-end'>NT$ 220</span>
-                                        </h5>
-                                        <button type='type' className="btn btn-outline-primary w-100 mt-2">
-                                            加入購物車
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col">
-                                <div className="card">
-                                    <img
-                                        src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
-                                        className="card-img-top" alt="..."/>
-                                    <div className="card-body">
-                                        <h5 className="card-title">
-                                            <span className='float-start'>全素食健康餐</span>
-                                            <span className='float-end'>NT$ 220</span>
-                                        </h5>
+                                        </h6>
                                         <button type='type' className="btn btn-outline-primary w-100 mt-2">
                                             加入購物車
                                         </button>
@@ -104,7 +104,9 @@ function App() {
                                             <a href="#">x</a>
                                         </td>
                                         <td>
-                                            <img src="" alt=""/>
+                                            <img src="https://images.unsplash.com/photo-1698620943272-59963ac1070f?auto=format&fit=crop&q=60&w=600&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0MXx8fGVufDB8fHx8fA%3D%3D"
+                                                 className='table-image'
+                                                 alt=""/>
                                         </td>
                                         <td>
                                             素食餐
