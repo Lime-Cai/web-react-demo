fee5f0960bfd2cb3450221bb032d67cfe26a9cbe
Lime <zong64066406@gmail.com>
2023-11-07 20:18:15 +0800

    CRA整合製作簡易購物車 - 修改購物車品項數量

--- src/App.js
+++ src/App.js
@@ -11,19 +11,24 @@ function App() {
     const cartReducer = useReducer((prevState, action) => {
         const carList = [...prevState.carList];
         // 取得當前購物車目標品項的索引
-        const index = carList.findIndex((item)=> item.id === action.payload.id)
+        const index = carList.findIndex((item) => item.id === action.payload.id)
         switch (action.type) {
             case 'ADD_TO_CART':
-                if (index === -1){
+                if (index === -1) {
                     // 還未加入到購物車內
                     carList.push(action.payload);
-                }else {
+                } else {
                     // 當前購物車的項目和加入的項目一致
                     carList[index].quantity += action.payload.quantity;
                 }
                 return {
                     ...prevState, carList
                 };
+            case 'CHANGE_CART_QUANTITY':
+                carList[index].quantity = action.payload.quantity;
+                return {
+                    ...prevState, carList
+                }
             default:
                 return prevState
         }
